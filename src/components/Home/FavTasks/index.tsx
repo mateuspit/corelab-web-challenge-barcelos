5faef6333be67e00e0f64be955eff65aeb0d685e
Mateus Barcelos <mateuspit@hotmail.com>
2023-10-03 17:17:32 -0300

    feat: new task and favs taks with db done

--- src/components/Home/FavTasks/index.tsx
+++ src/components/Home/FavTasks/index.tsx
@@ -1,8 +1,38 @@
+import "../../../styles/layout/_fav-tasks-home-component.sass"
+import { BsStarFill } from "react-icons/bs"
+import { LuPaintBucket } from "react-icons/lu"
+import { BsPencil } from "react-icons/bs"
+import { AiOutlineClose } from "react-icons/ai";
+
 export default function FavTasks() {
 
     return (
-        <div id="fav-tasks">
-            <h1>TASKS FAVORITAS AQUI</h1>
-        </div>
+        <>
+            <p id='fav-section'>Favoritas</p>
+            <div id="fav-tasks-container">
+                <div id="header-task">
+                    <p id="title-task">
+                        TITULO DA TASK
+                    </p>
+                    <div id="fav-icon" >
+                        <BsStarFill style={{ color: 'yellow', fontSize: '20px' }} />
+                    </div>
+                </div>
+                <div id="task-content">
+                    <p>
+                        CONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASKCONTEUDO DA TASK
+                    </p>
+                </div>
+                <div id="task-footer">
+                    <div id="footer-left-side">
+                        <p><LuPaintBucket /></p>
+                        <p id="edit-icon"><BsPencil /></p>
+                    </div>
+                    <div id="footer-right-side">
+                        <p><AiOutlineClose /></p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
--- src/components/Home/NewTask/index.tsx
+++ src/components/Home/NewTask/index.tsx
@@ -1,8 +1,67 @@
+import { BsStarFill, BsStar } from "react-icons/bs";
+import { LuPaintBucket } from "react-icons/lu";
+import "../../../styles/layout/_new-task-home-components.sass"
+import "../../../styles/utils/_variables.sass"
+import { useState, ChangeEvent } from "react";
+
 export default function NewTask() {
+
+    function createTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        alert("create");
+        setInputValue('')
+        setTaskValue('')
+        setIsFavStar(false)
+    }
+
+    const [inputValue, setInputValue] = useState<string>('');
+    const [taskValue, setTaskValue] = useState<string>('');
+    const [isFavStar, setIsFavStar] = useState<boolean>(false);
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        setInputValue(event.target.value);
+    }
+
+    function handleTextChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        setTaskValue(event.target.value);
+    }
+
+    function toggleIcon() {
+        setIsFavStar(!isFavStar);
+    }
 
     return (
         <div id="new-task">
-            <h1>NOVA TASK AQUI</h1>
+            <form onSubmit={createTask}>
+                <div id="header-task">
+                    <div id="title-task">
+                        <input
+                            type="text"
+                            value={inputValue}
+                            onChange={handleInputChange}
+                            placeholder="Título"
+                            id='my-input'
+                        />
+                    </div>
+                    <div id="fav-icon" onClick={toggleIcon}>
+                        {isFavStar ? <BsStarFill style={{ color: 'yellow', fontSize: '20px' }} /> : <BsStar style={{ color: 'black', fontSize: '20px' }} />}
+                    </div>
+                </div>
+                <div id="task-content">
+                    <textarea
+                        id="task-content"
+                        placeholder="Criar nota..."
+                        rows={4}
+                        cols={50}
+                        value={taskValue}
+                        onChange={handleTextChange}
+                    />
+                </div>
+                <div id="task-footer">
+                    <p><LuPaintBucket /></p>
+                    <button id="submit-button" type="submit" name="submitButton" >Criar nota</button>
+                </div>
+            </form>
         </div>
     )
 }
