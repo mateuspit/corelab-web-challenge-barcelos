e8cd7e127f4336c85fe0bf60767b15914d91e7f1
Mateus Barcelos <mateuspit@hotmail.com>
2023-10-03 14:19:16 -0300

    feat: header component in home done

--- src/components/Home/Header/index.tsx
+++ src/components/Home/Header/index.tsx
@@ -15,10 +15,15 @@ export default function Header() {
         'Cinza',
         'Marrom'];
 
-    const [selectedColor, setSelectedColor] = useState<string>('');
+    const [selectedColor, setSelectedColor] = useState<string>("");
+    const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
     const handleColorChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedColor(event.target.value);
+    };
+
+    const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsFavorite(event.target.checked);
     };
 
     return (
@@ -27,11 +32,17 @@ export default function Header() {
                 <img src="https://i.imgur.com/sY8bF7f.png" alt="logo" />
                 <p>CoreNotes</p>
             </div>
+
             <div id='search-container'>
                 <p>Filtre suas notas:</p>
 
-
-                <input type="checkbox" id="scales" name="scales" />
+                <input
+                    type="checkbox"
+                    id="scales"
+                    name="scales"
+                    checked={isFavorite}
+                    onChange={handleCheckboxChange}
+                />
                 <label htmlFor="scales">Favoritos</label>
 
                 <select
@@ -40,15 +51,13 @@ export default function Header() {
                     onChange={handleColorChange}
                     style={{ fontSize: '12px' }}
                 >
-                    <option value="">Selecione uma cor</option>
+                    <option value="">Cor da nota</option>
                     {colors.map((color, index) => (
                         <option key={index} value={color}>
                             {color}
                         </option>
                     ))}
                 </select>
-
-
             </div>
         </div>
     )
